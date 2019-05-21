29c69ae7441e20a1207da903f9f3f4f7fd1d6f1c
Dennis Hermsmeier <d.hermsmeier@mittwald.de>
2019-05-21 14:22:24 +0200

    higher timeout to 30s

--- probe/probe_mongodb.go
+++ probe/probe_mongodb.go
@@ -46,7 +46,7 @@ func (m *mongoDBProbe) Exec() error {
 		return err
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	err = client.Connect(ctx)
