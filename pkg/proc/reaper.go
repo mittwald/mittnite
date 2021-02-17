9d484d8a108220baa7f2df0cbd440bb492b81fe1
Henning Surmeier <h.surmeier@mittwald.de>
2021-02-17 14:51:32 +0100

    reap children since mittnite runs as pid 1
    https://blog.phusion.nl/2015/01/20/docker-and-the-pid-1-zombie-reaping-problem/

---
+++ pkg/proc/reaper.go
@@ -0,0 +1,33 @@
+package proc
+
+import (
+	log "github.com/sirupsen/logrus"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func ReapChildren() {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGCHLD)
+
+	for {
+		sig := <-signals
+		log.WithField("signal", sig).Info("handling signal")
+		for {
+			var s syscall.WaitStatus
+
+			pid, err := syscall.Wait4(-1, &s, 0, nil)
+			for syscall.EINTR == err {
+				pid, err = syscall.Wait4(pid, &s, 0, nil)
+			}
+
+			if syscall.ECHILD == err {
+				break
+			}
+
+			log.WithField("pid", pid).WithField("status", s).
+				Info("reaped child")
+		}
+	}
+}
