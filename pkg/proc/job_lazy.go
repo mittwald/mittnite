99ca67491559333a8c86f1fea18a636515e8bfe6
Henning <h.surmeier@mittwald.de>
2021-10-15 16:17:31 +0200

    use non blocking channel write for startOnce err (#35)
    * use non blocking channel write for startOnce err
    
    startOnce will most likely exit once the wrapping method has already
    returned, having written to job.process in the select statement.
    
    This results in the err channel being left without readers, therefore
    the go func will never exit if startOnce throws an error, as it can't write to e.
    
    * remove fmt

--- pkg/proc/job_lazy.go
+++ pkg/proc/job_lazy.go
@@ -29,7 +29,10 @@ func (job *LazyJob) AssertStarted(ctx context.Context) error {
 
 	go func() {
 		if err := job.startOnce(ctx, p); err != nil {
-			e <- err
+			select {
+			case e <- err:
+			default:
+			}
 		}
 
 		job.process = nil
@@ -72,7 +75,7 @@ func (job *LazyJob) Run(ctx context.Context, errors chan<- error) error {
 }
 
 func (job *LazyJob) startProcessReaper(ctx context.Context) {
-	ticker := time.NewTicker(5 * time.Minute)
+	ticker := time.NewTicker(1 * time.Minute)
 	go func() {
 		for {
 			select {
@@ -85,6 +88,10 @@ func (job *LazyJob) startProcessReaper(ctx context.Context) {
 
 				diff := time.Since(job.lastConnectionClosed)
 				if diff < job.coolDownTimeout {
+					continue
+				}
+
+				if job.process == nil {
 					continue
 				}
 
