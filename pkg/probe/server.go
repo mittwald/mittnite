0342cb955dd7466dcc038610d6e94f2127493389
Damian Torrie <d.torrie@mittwald.de>
2024-07-24 13:42:32 +0200

    Added PathError check

--- pkg/probe/server.go
+++ pkg/probe/server.go
@@ -33,6 +33,11 @@ func (h *Handler) Wait(interrupt chan os.Signal) error {
 
 			for i := range h.waitProbes {
 				err := h.waitProbes[i].Exec()
+				var pathErr *os.PathError
+				if errors.As(err, &pathErr) {
+					log.WithFields(log.Fields{"kind": "probe", "name": i, "err": err}).Fatal("Path does not exist!")
+					return nil
+				}
 				if err != nil {
 					log.WithFields(log.Fields{"kind": "probe", "name": i, "err": err}).Warn("not ready yet")
 					ready = false
